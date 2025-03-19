3a1130d774fa72573b9b79fff1cc61700ee77065
dTop <jroney002@gmail.com>
2025-03-19 15:57:48 -0500

    setting up thoughts table

--- starter/server/db/CreateThoughtsTable.js
+++ starter/server/db/CreateThoughtsTable.js
@@ -1,2 +1,21 @@
-connt AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
+AWS.config.update({ region: 'us-east-2' });
+
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+const params = {
+    TableName: 'Thoughts',
+    KeySchema: [
+        { AttributeName: 'username', KeyType: 'HASH' }, // Partition Key
+        { AttributeName: 'createdAt', KeyType: 'RANGE' }, // Sort Key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'username', AttributeType: 'S' },
+        { AttributeName: 'createdAt', AttributeType: 'N' },
+    ],
+    ProvisionedThroughout: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10,
+    },
+};
